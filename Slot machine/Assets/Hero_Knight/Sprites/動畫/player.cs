e9cf2410e3127541dba2bc2008ae62ea01bc671f
lockb9127 <66513347+lockb9127@users.noreply.github.com>
2020-09-14 21:26:19 +0800

    2

--- Slot machine/Assets/Hero_Knight/Sprites/動畫/player.cs
+++ Slot machine/Assets/Hero_Knight/Sprites/動畫/player.cs
@@ -14,6 +14,9 @@ public class player : MonoBehaviour
     public Bounds localBounds;
     public Rigidbody2D rig;
     public Transform tra;
+    public float jumpforec;
+
+    public bool isground;
 
     public void Attck()
     {
@@ -22,12 +25,32 @@ public class player : MonoBehaviour
 
 
     }
+    public void Jump()
+    {
+        if (isground)
+
+        {
+
+            if (Input.GetButtonDown("Vertical"))
+            {
+                rig.velocity = new Vector2(rig.velocity.x, jumpforec * Time.deltaTime);
+
+            }
+
+           
+        }
+    }
     public void Move()
     {
         float v = Input.GetAxis("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
         Vector3 pos = new Vector3(v,0,0);
         rig.MovePosition(transform.position + pos * speed);
         ani.SetFloat("跑", Mathf.Abs(v));
+        if(h !=0)
+        {
+            transform.localScale = new Vector3(h*3, 3, 3);
+        }
 
     }
     public void Hit()
@@ -51,5 +74,14 @@ public class player : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name=="地板")
+        {
+            isground = true;
+        }
+       
     }
 }
